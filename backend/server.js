c55f3b6952fc35cd862543a3901b1e3025d68729
Alex Oluwatimilehin <officialalexoluwatimilehin@gmail.com>
2026-08-19 16:38:58 +0100

    Fix frontend backend integration

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(require("path").join(__dirname, "..")));
 
 app.use("/api", authRoutes);
 
--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "/api";
